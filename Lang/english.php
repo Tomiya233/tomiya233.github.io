b9cc954bee7f8d8ecb2e7c084b95282c677f5222
Tomiya233 <85816199+Tomiya233@users.noreply.github.com>
2021-06-13 20:38:31 +0800

    Add files via upload

---
+++ Lang/english.php
@@ -0,0 +1,318 @@
+<?php
+// 语言 - 中文，表面上，这个文件原本是英文语言，不过本人懒得改太多.
+// *************        汉化包         *************/
+// *************      战神无限制作     *************/
+// *************     QQ：605853742     *************/
+// **********以上是广告，版权不归汉化者所有.********/
+// 如果有不正确的地方，欢迎修改，本人英文水平不怎么样
+// ************最后说一句，汉化真的很辛苦***********/
+// Main Page /pages/upload.php（上传页面）
+$lang[upload] = "上传" ;
+$lang[maxsize] = "最大文件大小:";
+$lang[email] = "Email地址:";
+$lang[description] = "文件简介:";
+$lang[protection] = "密码保护:";
+$lang[sinfo] = "上传文件表示您同意";
+$lang[tos] = "服务条款";
+$lang[progress] = "上传进度...";
+$lang[hosting] = "目前存储";
+$lang[files] = "文件夹";
+$lang[total] = "总数!";
+$lang[info] = "是一个免费的文件托管，用一个简单易用的方式来分享您的文件的服务！要开始，选择一个文件...";
+$lang[nofile] = "请选择要上传的文件!";
+$lang[emailopt] = "Email";
+$lang[desopt] = "文件简介";
+$lang[passopt] = "密码保护";
+$lang[opt] = "可选";
+$lang[moreoptions] = "更多选项";
+
+// adminlinks.php（管理员界面）
+$lang[downloads] = "下载" ;
+$lang[stats] = "统计" ;
+$lang[edit] = "编辑" ;
+
+// Upload Page upload.php（上传界面-2）
+$lang[yupfile] = "您的文件已成功上传!";
+$lang[udownfile] = "文件下载链接";
+$lang[udeletefile] = "文件删除链接";
+$lang[uremfile] = "请记住链接地址.";
+$lang[ufile] = "您的文件";
+$lang[thank] = "感谢您使用我们的文件存储服务!";
+$lang[yup] = "您上传的文件";
+$lang[wup] = "已上传";
+$lang[tsoon] = "您必须等待一段时间，然后才可以上传其他文件!";
+$lang[tlarge] = "您上传的文件太大";
+$lang[dpick] = "您没有选择要上传的文件";
+$lang[icat] = "无效的类型..";
+$lang[itype] = "此类型的文件禁止上传.";
+$lang[nallow] = "您不能上传此类型的文件.";
+$lang[noallow] = "此文件不允许被上传.";
+$lang[file_upload] = "文件 - 上传";
+
+// Top 10 Page top.php（文件排行榜）
+$lang[fname] = "名字";
+$lang[dloads] = "下载";
+$lang[disabled10] = "此页面不存在.";
+$lang[size10] = "大小";
+$lang[ldload] = "最后下载";
+$lang[fdis10] = "Top 10 列表不存在.";
+$lang[topten] = "Top 10 文件";
+
+// report.php（文件举报页面）
+$lang[treport] = "举报此文件.";
+$lang[uall] = "您不能举报此文件.";
+$lang[frepo] = "文件已举报.感谢您的参与";
+$lang[redir] = "10秒后返回主页...";
+$lang[rthanks] = "文件已举报.";
+
+// Index.php（应该是首页）
+$lang[allfile] = "允许上传的文件类型:";
+$lang[cat2] = "类别";
+
+// files.php（文件列表页）
+$lang[filelist] = "文件列表";
+$lang[disfiles] = "此页面不存在";
+$lang[fldis] = "文件列表页不存在.";
+$lang[ftotal] = " 文件总数: ";
+
+// top.php（头部）
+$lang[topfiles] = "最新上传的文件";
+$lang[disfiles] = "此页面不存在";
+$lang[fldis] = "文件列表页不存在.";
+
+// download.php
+$lang[younallow] = "你不能下载文件.";
+$lang[inlink] = "无效的下载链接.";
+$lang[indlink2] = "删除无效链接.";
+$lang[ufwd] = "文件已被删除.";
+$lang[uwbr] = "10秒后返回主页.";
+$lang[pw2] = "密码保护";
+$lang[petc] = "请输入正确的密码后才能下载";
+$lang[fd6] = "文件说明";
+$lang[fn6] = "文件名";
+$lang[tdsoon] = "上传文件的速度太快了!";
+$lang[rtf] = "举报此文件";
+$lang[fbu] = "文件下载流量";
+$lang[nenjava] = "如果您看到此消息，请启用JS，否则无法正常下载.";
+$lang[dl_ip] = "IP 地址";
+$lang[dl_filesize] = "文件大小";
+$lang[dl_file_dl] = "文件下载";
+$lang[dl_file_dl1] = "时间";
+$lang[dl_last_dl] = "最后下载";
+$lang[dl_file_now] = "立即下载文件！";
+$lang[dl_file_now1] = "请稍等";
+$lang[dl_file_now2] = "秒";
+$lang[dl_ticket] = "下载缓冲时间";
+$lang[dl_a_file] = "从服务器下载此文件";
+
+// download.php + download2.php（D1和D2的下载页面）
+$lang[younallow] = "禁止下载此文件.";
+$lang[inlink] = "无效的下载链接.";
+$lang[indlink2] = "无效的删除链接.";
+$lang[ufwd] = "文件被删除.";
+$lang[uwbr] = "10秒后返回主页.";
+$lang[pw2] = "密码保护";
+$lang[petc] = "输入正确的密码才能下载";
+$lang[fd6] = "文件说明";
+$lang[fn6] = "文件名";
+$lang[tdsoon] = "请不要过快上传!";
+$lang[rtf] = "举报此文件";
+$lang[fbu] = "文件已用流量";
+$lang[nenjava] = "如果您看到此消息，您需要启用JavaScript";
+
+// Adminpanel（管理员面板）
+
+// Header-Links（头部导航）
+$lang[logout] = "安全退出";
+$lang[index] = "主页";
+$lang[files] = "文件";
+$lang[master] = "管理员密码";
+$lang[abuse] = "Abuse（举报）";
+$lang[delete] = "删除文件";
+$lang[bans] = "Bans(禁令)";
+$lang[info_1] = "服务器信息";
+$lang[check] = "文件检查";
+$lang[settings] = "系统设置";
+
+// Admin-Index
+$lang[welcome] = "欢迎登录";
+$lang[adminpanel] = "管理员面板";
+$lang[adel] = "删除";
+$lang[statistic] = "统计:";
+$lang[diskspace] = "磁盘总空间:";
+$lang[in_use] = "已用空间:";
+$lang[free] = "可用空间:";
+$lang[adminlogin] = "密码: ";
+$lang[adminlogout] = "成功注销退出!";
+$lang[bandwith] = "流量";
+$lang[set_master] = "在这里您可以为所有文件设置一个下载主密码。要小心使用此选项!";
+$lang[set_master_now] = "现在设置主密码!";
+$lang[set_master_1] = "输入管理员密码: ";
+$lang[ignore] = "忽略报告";
+$lang[abuse_man] = "举报管理";
+$lang[ban_text] = "输入IP或禁止文件: ";
+$lang[total_files] = "文件总数:";
+$lang[total_filesize] = "文件总容量:";
+$lang[total_downloads] = "总下载数:";
+$lang[total_bandwith] = "总下载流量:";
+$lang[check_1] = "检查系统";
+$lang[check_2] = "请设置所有文件夹和文本文件为可写入（文件模式0777）";
+$lang[folder] = "文件夹";
+$lang[textfiles] = "文本文件";
+$lang[foldern] = "文件夹不存在";
+$lang[foldere] = "文件夹存在，并且是可写的";
+$lang[foldernw] = "文件夹不可写。 （设置chmod 0777）";
+$lang[fe] = "文件存在并且是可写的";
+$lang[few] = "文件不存在或不可写。 （设置chmod 0777）";
+$lang[rp] = "刷新页面";
+$lang[info_11] = "服务器信息";
+$lang[la] = "最后更新";
+$lang[sd] = "服务器和服务";
+$lang[ap] = "处理器负载: ";
+$lang[cpu] = "处理器负载 ";
+$lang[pws] = "密码";
+$lang[downloader] = "下载";
+$lang[visitor] = "访客:";
+$lang[adminlogs] = "管理员日志: (进入后台管理)";
+$lang[deleteadminlogs] = "删除管理员日志";
+
+// settings.php（系统设置页面）
+$lang[changed_success] = "设置成功保存";
+$lang[change_not_sucess] = "保存失败";
+$lang[settings_admin] = "管理员链接";
+$lang[settings_admin1] = "显示管理员链接";
+$lang[settings_on] = "开";
+$lang[settings_off] = "关";
+$lang[settings_change] = "更改设置";
+$lang[settings_minutes] = "分钟";
+$lang[settings_seconds] = "秒";
+$lang[settings_days] = "天";
+$lang[settings_site_name] = "网站名称";
+$lang[settings_slogan] = "关键字";
+$lang[settings_script_url] = "Script URL";
+$lang[settings_password] = "管理员密码";
+$lang[settings_email] = "您的邮箱地址";
+$lang[settings_language] = "语言";
+$lang[settings_maxfilesize] = "最大文件大小";
+$lang[settings_downloadtimelimit] = "下载时间限制";
+$lang[settings_uploadtimelimit] = "上传时间限制";
+$lang[settings_nolimitsize] = "不限制下载时间";
+$lang[settings_deleteafter] = "文件自动销毁限时";
+$lang[settings_downloadtimer] = "下载定时";
+$lang[settings_filelist] = "启用文件列表";
+$lang[settings_shorturl] = "下载地址优化";
+$lang[settings_email_option] = "邮箱设置";
+$lang[settings_password_feature] = "密码功能";
+$lang[settings_file_description] = "文件说明";
+$lang[settings_toplist] = "Top 10 列表";
+$lang[settings_site_name1] = "站点名称";
+$lang[settings_slogan1] = "站点关键字";
+$lang[settings_script_url1] = "请输入网站的URL在这里以斜线结束 '/'";
+$lang[settings_password1] = "请在这里输入您的管理员的密码";
+$lang[settings_password2] = "!!! 首先请更改 !!!";
+$lang[settings_email1] = "这里设定您的电子邮件Adress";
+$lang[settings_language1] = "这是您要的站点语言（一般不用改）";
+$lang[settings_maxfilesize1] = "最大文件大小（如： 25是指25MB）";
+$lang[settings_downloadtimelimit1] = "用户下载另一个文件必须等待的时间";
+$lang[settings_uploadtimelimit1] = "用户上传另一个文件必须等待的时间";
+$lang[settings_nolimitsize1] = "如果文件的下载流量达到这么多MB，就没有时间限制，文件永久保存.";
+$lang[settings_deleteafter1] = "如果这么多天之后没有下载删除文件";
+$lang[settings_downloadtimer1] = "下载页的下载等待时间计数器";
+$lang[settings_filelist1] = "允许用户看到一个上传文件的列表。设置为“关”停用";
+$lang[settings_shorturl1] = "短网址例如：yourdomain.com/13232需要mod_rewrite的启用（IIS主机不要启用！）";
+$lang[settings_email_option1] = "将此项设置为开，允许用户自己设置发送下载链接的电子邮件";
+$lang[settings_password_feature1] = "将此项设置为开，允许用户设置下载密码";
+$lang[settings_file_description1] = "将此项设置为开，允许设置文件简介";
+$lang[settings_toplist1] = "允许用户看到的10个文件顶部的列表。设置为”关“停用";
+$lang[settings_dlspeed] = "下载速度";
+$lang[settings_dlspeed1] = "在这里设置你的下载速度";
+$lang[settings_pps] = "每页显示的数目";
+$lang[settings_pps1] = "显示多少文件";
+$lang[settings_pps2] = "每页";
+$lang[settings_pps3] = "显示多少用户";
+$lang[settings_pps4] = "显示多少下载列表";
+$lang[settings_aemail] = "联系邮件";
+$lang[settings_aemail1] = "在这里设置你的联系邮件";
+$lang[settings_personal_data] = "您的个人资料设置";
+$lang[settings_personal_data1] = "请详细填写，有助于提高收录";
+$lang[settings_personal_name] = "站点名";
+$lang[settings_personal_name1] = "站点名字";
+$lang[settings_personal_street] = "地址";
+$lang[settings_personal_street1] = "公司地址";
+$lang[settings_personal_city] = "邮编与城市";
+$lang[settings_personal_city1] = "您的邮编与城市";
+$lang[settings_personal_phone] = "电话号码";
+$lang[settings_personal_phone1] = "您的电话号码";
+$lang[settings_personal_url] = "网址";
+$lang[settings_personal_url1] = "站点地址";
+$lang[settings_main_config] = "主要设置";
+$lang[dldate] = "日期";
+$lang[dltime] = "时间";
+$lang[dlback] = "返回到文件";
+$lang[settings_style] = "风格";
+$lang[settings_style1] = "在此设置系统风格";
+$lang[dltotal] = "下载总量: ";
+$lang[settings_multiupload] = "分流上传";
+$lang[settings_multiupload1] = "上传时就会为你把文件分流到其他的服务器，节省你的时间及带宽";
+$lang[settings_search] = "搜索功能";
+$lang[settings_search1] = "显示搜索链接，允许用户搜索已上传文件";
+$lang[settings_enhanced] = "增强型上传";
+$lang[settings_enhanced1] = "进度条，多文件，多服务器，验证码 - 需要PERL的支持";
+$lang[settings_securitycode] = "验证码上传";
+$lang[settings_securitycode1] = "想要上传文件就必须输入验证码";
+$lang[settings_pps5] = "每页点击在搜索页";
+$lang[settings_pfolder] = "我的文件夹";
+$lang[settings_pfolder1] = "显示个人文件夹链接";
+$lang[settings_multiserver] = "多服务器";
+$lang[settings_multiserver1] = "服务器名+ Script url （多服务器设定）";
+$lang[settings_multiserver_config] = "多服务器 - 配置";
+$lang[settings_zusatz_config] = "扩展配置";
+$lang[settings_cgi_config] = "cgi - bin目录配置";
+$lang[settings_uscript] = "上传- Script";
+$lang[settings_uscript1] = "路径上传  - Script";
+$lang[settings_pscript] = "进度 Script";
+$lang[settings_pscript1] = "路径进度 - Script";
+$lang[settings_configfile] = "CGI的配置文件";
+$lang[settings_configfile1] = "CGI执行配置文件（请名手动更改设置）";
+
+// bl.php（不知某页面）
+$lang[pagination] = "页面: ";
+
+// Toplinks（头部链接）
+$lang[top_upload] = "主页";
+$lang[top_files] = "文件";
+$lang[top_tos] = "服务条款";
+$lang[top_faq] = "帮助";
+$lang[top_top] = "文件排行榜";
+$lang[top_admin] = "管理";
+
+// tos.php（服务条款）
+$lang[tos_tos] = "服务条款";
+$lang[tos_point1] = "你不能上载任何侵犯版权的文件.";
+$lang[tos_point2] = "你不能上传任何被认为在您的国家非法的文件。";
+$lang[tos_point3] = "您必须为您上传的文件负责.";
+$lang[tos_point4] = "本网站保留以任何理由删除所有者在这里上传的任何文件的权利..";
+$lang[tos_send_abuse] = "发送联系链接: ";
+
+// faq.php（在线帮助）
+$lang[faq] = "在线帮助";
+$lang[faq1] = "我可以上传多大的文件？";
+$lang[faq2] = "我的文件可以保存多长时间.?";
+$lang[faq3] = "上传和下载文件的限制是什么？";
+$lang[faq4] = "较小的文件怎么样？";
+$lang[faq1a] = "允许的最大文件大小是";
+$lang[faq2a] = "您的文件将被保存在长时间我们的服务器，只要它是经常被下载.";
+$lang[faq2b] = "天";
+$lang[faq3a] = "你可以上传一个文件每隔";
+$lang[faq3b] = "分钟.";
+$lang[faq3c] = "你可以下载一个文件每隔";
+$lang[faq3d] = "分钟.";
+$lang[faq4a] = "文件小于";
+$lang[faq4b] = "MB将解除你的上传或下载的限制。";
+
+// search.php（搜索页面）
+$lang[search] = "文件搜索";
+$lang[searchhits] = "找到文件";
+$lang[nosearchhits] = "抱歉，没有找到文件";
+
+?>
---
+++ Lang/german.php
@@ -0,0 +1,317 @@
+<?php
+// Language - German
+
+// Main Page
+$lang[upload] = "Upload" ;
+$lang[maxsize] = "Maximale Dateigröße :";
+$lang[email] = "E-Mail Addresse :";
+$lang[description] = "Datei Beschreibung :";
+$lang[protection] = "Passwortschutz :";
+$lang[sinfo] = "Mit dem Hochladen akzeptieren Sie unsere";
+$lang[tos] = "AGBs & Imprint";
+$lang[progress] = "Es wird gerade hochgeladen. Bitte warten...";
+$lang[hosting] = "Wir hosten";
+$lang[files] = "Dateien";
+$lang[total] = "Gesamt!";
+$lang[info] = "ist ein kostenloser Dateihosting-Service. Laden Sie Ihre Dateien kostenlos hoch! Suchen Sie eine Datei von Ihrem System und laden diese hoch. Optional können Sie sich Ihren Download- und Löschlink per E-Mail zusenden lassen.";
+$lang[nofile] = "Bitte suchen Sie eine Datei zum hochladen.";
+$lang[emailopt] = "E-Mail";
+$lang[desopt] = "Datei-Beschreibung";
+$lang[passopt] = "Passwortschutz";
+$lang[opt] = "Optional";
+$lang[moreoptions] = "Mehr Optionen";
+$lang[prelogin] = "Login";
+$lang[prepass] = "Passwort";
+
+// adminlinks.php
+$lang[downloads] = "Dls" ;
+$lang[stats] = "Statistik" ;
+$lang[edit] = "Edit" ;
+
+// Upload Page upload.php
+$lang[yupfile] = "Ihre Datei wurde hochgeladen!";
+$lang[udownfile] = "Ihr Download Link";
+$lang[udeletefile] = "Ihr Lösch Link";
+$lang[uremfile] = "Bitte merken Sie sich diese Links gut.";
+$lang[ufile] = "Ihre Datei";
+$lang[thank] = "Danke dass Sie unseren Service benutzten.";
+$lang[yup] = "Ihre ausgewählte Datei";
+$lang[wup] = "ist hochgeladen";
+$lang[tsoon] = "You're trying to upload again too soon!";
+$lang[tlarge] = "Die Datei ist zu groß";
+$lang[dpick] = "Sie haben keine Datei ausgewählt";
+$lang[icat] = "Falsche Kategorie";
+$lang[itype] = "Dieser Dateityp ist leider nicht erlaubt.";
+$lang[nallow] = "Sie sind nicht berechtigt, Dateien hochzuladen.";
+$lang[noallow] = "Diese Datei dürfen Sie nicht hochladen.";
+$lang[file_upload] = "Datei - Upload";
+
+// Top 10 Page top.php
+$lang[fname] = "Dateiname";
+$lang[dloads] = "Downloads";
+$lang[disabled10] = "Diese Seite ist deaktiviert.";
+$lang[size10] = "Größe";
+$lang[ldload] = "Letzter Download";
+$lang[fdis10] = "Top 10 Liste ist deaktiviert.";
+$lang[topten] = "Top 10 Dateien";
+
+// report.php
+$lang[treport] = "Melden Sie einen Dateiverstoß.";
+$lang[uall] = "Sie haben keine Berechtigung, Dateien zu melden.";
+$lang[frepo] = "File reported. Thanks.";
+$lang[redir] = "Sie werden in 10 Sekunden zur Startseite weitergeleitet.";
+$lang[rthanks] = "Datei gemeldet. Danke.";
+
+// Index.php
+$lang[allfile] = "Erlaubte Dateitypen:";
+$lang[cat2] = "Kategorie";
+
+// files.php
+$lang[filelist] = "Datei Liste";
+$lang[disfiles] = "Diese Seite ist deaktiviert";
+$lang[fldis] = "Die Dateiliste ist deaktiviert.";
+$lang[ftotal] = " Gesamte Dateien: ";
+
+// top.php
+$lang[topfiles] = "Top 10 Dateien";
+$lang[disfiles] = "Diese Seite ist deaktiviert";
+$lang[fldis] = "Die Dateiliste ist deaktiviert.";
+
+// download.php
+$lang[younallow] = "Sie haben keine Berechtigung Dateien herunterzuladen.";
+$lang[inlink] = "Falscher Download-Link.";
+$lang[indlink2] = "Falscher Lösch-Link.";
+$lang[ufwd] = "Ihre Datei wurde gelöscht.";
+$lang[uwbr] = "Sie werden in 10 Sekunden zur Startseite weitergeleitet.";
+$lang[pw2] = "Passwortgesichert";
+$lang[petc] = "Bitte geben Sie das korrekte Passwort ein, um die Datei herunterzuladen";
+$lang[fd6] = "Dateibeschreibung";
+$lang[fn6] = "Dateiname";
+$lang[tdsoon] = "Sie haben versucht, zu früh wieder hochzuladen!";
+$lang[rtf] = "Diese Datei melden";
+$lang[fbu] = "Traffic dieser Datei";
+$lang[nenjava] = "Wenn Sie diese Meldung sehen, müssen sie JavaScript in Ihrem Browser aktivieren";
+$lang[dl_ip] = "IP Adresse";
+$lang[dl_filesize] = "Dateigröße";
+$lang[dl_file_dl] = "Schon heruntergeladen";
+$lang[dl_file_dl1] = "x";
+$lang[dl_last_dl] = "Letzter Download";
+$lang[dl_file_now] = "Datei jetzt herunterladen";
+$lang[dl_file_now1] = "Bitte warten Sie noch";
+$lang[dl_file_now2] = "Sekunden";
+$lang[dl_ticket] = "Download Ticket reserviert";
+$lang[dl_a_file] = "Eine Datei von unserem Server herunterladen";
+
+// download.php + download2.php
+$lang[younallow] = "Sie haben keine Berechtigung, Dateien herunterzuladen.";
+$lang[inlink] = "Ungültiger Download-Link.";
+$lang[indlink2] = "Ungültiger Lösch-Link.";
+$lang[ufwd] = "Ihre Datei wurde gelöscht.";
+$lang[uwbr] = "Sie werden in 10 Sekunden zur Startseite weitergeleitet.";
+$lang[pw2] = "Passwortgesichert";
+$lang[petc] = "Bitte geben sie das korrekte Passwort ein, um diese Datei herunterzuladen";
+$lang[fd6] = "Dateibeschreibung";
+$lang[fn6] = "Dateiname";
+$lang[tdsoon] = "Sie haben versucht, zu früh wieder hochzuladen!";
+$lang[rtf] = "Diese Datei melden";
+$lang[fbu] = "Traffic dieser Datei";
+$lang[nenjava] = "Wenn Sie diese Meldung sehen, müssen sie JavaScript in Ihrem Browser aktivieren";
+
+// Adminpanel
+
+// Header-Links
+$lang[logout] = "Logout";
+$lang[index] = "Home";
+$lang[files] = "Dateien";
+$lang[master] = "MasterPW";
+$lang[abuse] = "Abuse";
+$lang[delete] = "Löschen";
+$lang[bans] = "Bannen";
+$lang[info_1] = "Info";
+$lang[check] = "Prüfen";
+$lang[settings] = "Einstellungen";
+
+// Admin-Index
+$lang[welcome] = "Willkommen in Ihrem";
+$lang[adminpanel] = "Adminpanel";
+$lang[adel] = "Löschen";
+$lang[statistic] = "Statistik:";
+$lang[diskspace] = "Gesamter Speicherplatz:";
+$lang[in_use] = "Davon verbraucht:";
+$lang[free] = "Noch frei:";
+$lang[adminlogin] = "Passwort: ";
+$lang[adminlogout] = "Sie haben sich erfolgreich ausgeloggt!";
+$lang[bandwith] = "Traffic";
+$lang[set_master] = "Hier können Sie für alle Downloads ein einziges Master-Passwort setzen. Seien Sie vorsichtig mit dieser Option!!!";
+$lang[set_master_now] = "Masterpasswort jetzt setzen!!!";
+$lang[set_master_1] = "Master Passwort eingeben: ";
+$lang[ignore] = "Meldung ignorieren";
+$lang[abuse_man] = "Gemeldete Dateien (Abusereports)";
+$lang[ban_text] = "Geben Sie eine IP oder einen Datei-Hash ein zum Bannen: ";
+$lang[total_files] = "Gesamte Dateien:";
+$lang[total_filesize] = "Gesamte Dateigröße:";
+$lang[total_downloads] = "Gesamte Downloads:";
+$lang[total_bandwith] = "Gesamter Traffic:";
+$lang[check_1] = "Testen Sie Ihre MFH Installation";
+$lang[check_2] = "Bitte machen Sie alle Verzeichnisse und Textdateien beschreibbar. (CHMOD 0777)";
+$lang[folder] = "Verzeichnisse";
+$lang[textfiles] = "Textdateien";
+$lang[foldern] = "Verzeichnis existiert nicht";
+$lang[foldere] = "Verzeichnis existiert und ist beschreibbar";
+$lang[foldernw] = "Verzeichnis ist nicht beschreibbar. Bitte CHMOD 0777";
+$lang[fe] = "Datei existiert und ist beschreibbar";
+$lang[few] = "Datei existiert nicht oder ist nicht beschreibbar. Bitte CHMOD 0777";
+$lang[rp] = "Seite neu laden";
+$lang[info_11] = "Hier bekommen Sie aktuelle Infos zu Ihrem Server";
+$lang[la] = "Letzte Aktualisierung";
+$lang[sd] = "Server und Dienste";
+$lang[ap] = "Aktuelle Prozessorauslastung: ";
+$lang[cpu] = "CPU-Auslastung von ";
+$lang[pws] = "Passwort";
+$lang[downloader] = "Downloads";
+$lang[visitor] = "Besucher:";
+$lang[adminlogs] = "Adminlogs: (Zugriffe auf das Admin-Login)";
+$lang[deleteadminlogs] = "Adminlogs löschen";
+
+// settings.php
+$lang[changed_success] = "Einstellungen erfolgreich gespeichert";
+$lang[change_not_sucess] = "Einstellungen konnten nicht gespeichert werden";
+$lang[settings_admin] = "Admin-Link";
+$lang[settings_admin1] = "Zeigt den Admin-Link an";
+$lang[settings_on] = "An";
+$lang[settings_off] = "Aus";
+$lang[settings_change] = "Einstellungen ändern";
+$lang[settings_minutes] = "Minuten";
+$lang[settings_seconds] = "Sekunden";
+$lang[settings_days] = "Tagen";
+$lang[settings_site_name] = "Name der Seite";
+$lang[settings_slogan] = "Ihr Slogan";
+$lang[settings_script_url] = "Skript URL";
+$lang[settings_password] = "Admin Passwort";
+$lang[settings_email] = "Ihre eMail";
+$lang[settings_language] = "Sprache";
+$lang[settings_maxfilesize] = "Max. Dateigröße";
+$lang[settings_downloadtimelimit] = "DL Wartezeit";
+$lang[settings_uploadtimelimit] = "Upload Wartezeit";
+$lang[settings_nolimitsize] = "Kein Limit Größe";
+$lang[settings_deleteafter] = "Lösche nach";
+$lang[settings_downloadtimer] = "Download Timer";
+$lang[settings_filelist] = "Erlaube Dateiliste";
+$lang[settings_shorturl] = "Kurze URL";
+$lang[settings_email_option] = "eMail Option";
+$lang[settings_password_feature] = "Passwort Option";
+$lang[settings_file_description] = "Dateibeschreibung";
+$lang[settings_toplist] = "Top 10 Liste";
+$lang[settings_site_name1] = "Der Name Ihrer Seite";
+$lang[settings_slogan1] = "Der Slogan Ihrer Seite";
+$lang[settings_script_url1] = "Schließen Sie die URL mit einem Slash '/'";
+$lang[settings_password1] = "Setzen Sie hier Ihr Passwort";
+$lang[settings_password2] = "!!! Bitte ändern Sie dies zuerst !!!";
+$lang[settings_email1] = "Setzen Sie Ihre eMail-Adresse hier";
+$lang[settings_language1] = "Hier können Sie die Sprache Ihres MFH ändern";
+$lang[settings_maxfilesize1] = "Die maximal erlaubte Upload-Größe";
+$lang[settings_downloadtimelimit1] = "Die Zeit bevor ein User eine neue Datei herunterladen kann";
+$lang[settings_uploadtimelimit1] = "Die Zeit bevor ein User eine neue Datei hochladen kann";
+$lang[settings_nolimitsize1] = "Ist eine Datei unter dieser Größe, gibt es keine Zeit-Beschränkungen";
+$lang[settings_deleteafter1] = "Löscht Dateien nach X Tagen, wenn sie nicht heruntergeladen wurden";
+$lang[settings_downloadtimer1] = "Länge des Downloadtimers auf der Download-Seite";
+$lang[settings_filelist1] = "Zeigt eine Liste aller hochgeladenen Dateien an";
+$lang[settings_shorturl1] = "Kurze URL. Z.B.: domain.com/13232 - Benötigt mod_rewrite";
+$lang[settings_email_option1] = "Erlaubt Usern, sich Ihre Links per EMail zusenden zu lassen";
+$lang[settings_password_feature1] = "Erlaubt Usern, Ihre Dateien mit einem Download-Passwort zu schützen";
+$lang[settings_file_description1] = "Erlaubt Usern, eine Dateibeschreibung einzugeben";
+$lang[settings_toplist1] = "Erlaubt Usern, die Top 10 Dateien zu betrachten";
+$lang[settings_dlspeed] = "Download Speed";
+$lang[settings_dlspeed1] = "Geben Sie hier die Download Geschwindigkeit ein";
+$lang[settings_pps] = "Einträge";
+$lang[settings_pps1] = "Bestimmt die Anzahl der Einträge pro Seite im Admin-Bereich";
+$lang[settings_pps2] = "Pro Seite";
+$lang[settings_pps3] = "Bestimmt die Anzahl der Einträge pro Seite in der öffentlichen Dateiliste";
+$lang[settings_pps4] = "Bestimmt die Anzahl der Einträge pro Seite in der Downloadliste";
+$lang[settings_aemail] = "Abuse-eMail";
+$lang[settings_aemail1] = "Geben Sie hier ihre Abuse-eMail ein";
+$lang[settings_personal_data] = "Konfiguration Ihrer persönliche Daten";
+$lang[settings_personal_data1] = "(Für die AGB - Nicht für alle Länder, doch in Deutschland Pflicht)";
+$lang[settings_personal_name] = "Name";
+$lang[settings_personal_name1] = "Ihr Name";
+$lang[settings_personal_street] = "Strasse";
+$lang[settings_personal_street1] = "Ihre Strasse";
+$lang[settings_personal_city] = "PLZ & Stadt";
+$lang[settings_personal_city1] = "Ihre Postleitzahl und Stadt";
+$lang[settings_personal_phone] = "Telefonnummer";
+$lang[settings_personal_phone1] = "Ihre Telefonnummer";
+$lang[settings_personal_url] = "Internet";
+$lang[settings_personal_url1] = "Ihre Internetadresse";
+$lang[settings_main_config] = "Hauptkonfiguration";
+$lang[dldate] = "Datum";
+$lang[dltime] = "Zeit";
+$lang[dlback] = "Zurück zur Dateiliste";
+$lang[settings_style] = "Style";
+$lang[settings_style1] = "Ändere hier Deinen MFH Style";
+$lang[dltotal] = "Downloads gesamt: ";
+$lang[settings_multiupload] = "Multiupload";
+$lang[settings_multiupload1] = "Erlaubt Multiupload";
+$lang[settings_search] = "Suche";
+$lang[settings_search1] = "Zeige Suchlink an";
+$lang[settings_enhanced] = "Enhanced Upload";
+$lang[settings_enhanced1] = "Progress bar, multi-file, multi-server, and captcha - Requires PERL";
+$lang[settings_securitycode] = "Captcha Upload";
+$lang[settings_securitycode1] = "Zeige Captcha beim Upload an";
+$lang[settings_pps5] = "Ergebnisse pro Seite auf der Suchseite";
+$lang[settings_pfolder] = "Mein Ordner";
+$lang[settings_pfolder1] = "Persönlicher Ordner Link anzeigen";
+$lang[settings_multiserver] = "Multi-Server";
+$lang[settings_multiserver1] = "Servername + Scripturl (Multiserver-Einstellung)";
+$lang[settings_multiserver_config] = "Multiserver - Konfiguration";
+$lang[settings_zusatz_config] = "Erweiterte Konfiguration";
+$lang[settings_cgi_config] = "CGI-BIN Konfiguration";
+$lang[settings_uscript] = "Upload Script";
+$lang[settings_uscript1] = "Pfad zum Upload - Script";
+$lang[settings_pscript] = "Progress Script";
+$lang[settings_pscript1] = "Pfad zum Progress - Script";
+$lang[settings_configfile] = "CGI Config Datei";
+$lang[settings_configfile1] = "Name der CGI Configurations Datei (Einstellungen bitte manuell vornehmen)";
+
+// bl.php
+$lang[pagination] = "Seite: ";
+
+// Toplinks
+$lang[top_upload] = "Hochladen";
+$lang[top_files] = "Dateien";
+$lang[top_tos] = "A.G.B. & Imprint";
+$lang[top_faq] = "F.A.Q.";
+$lang[top_top] = "Top 10";
+$lang[top_admin] = "Admin";
+
+// tos.php
+$lang[tos_tos] = "Unsere AGB - Allgemeinen Geschäftsbedingungen & Impressum (Imprint)";
+$lang[tos_point1] = "Sie dürfen kein Copyright-geschütztes Material hochladen.";
+$lang[tos_point2] = "Sie dürfen kein Material hochladen, dass illegal oder verboten ist in Ihrem Land.";
+$lang[tos_point3] = "Sie sind alleinig für Ihre hochgeladenen Dateien verantwortlich.";
+$lang[tos_point4] = "Der Betreiber dieser Seite behält sich das Recht vor, Dateien ohne Ankündigung oder Begründung zu löschen.";
+$lang[tos_send_abuse] = "Senden Sie Ihre Abuse-Links zu: ";
+
+// faq.php
+$lang[faq] = "F.A.Q. - Häufig gestellte Fragen";
+$lang[faq1] = "Was ist die maximale Dateigröße zum hochladen?";
+$lang[faq2] = "Wie lange werden meine Dateien hier gehostet?";
+$lang[faq3] = "Gibt es Zeitlimits für das Hoch- und Herunterladen von Dateien?";
+$lang[faq4] = "Was ist mit kleineren Dateien?";
+$lang[faq5] = "Kann ich selbst so einen File Hosting Service betreiben?";
+$lang[faq1a] = "Die maximal erlaubte Dateigröße beträgt";
+$lang[faq2a] = "Ihre Dateien bleiben bei uns solange gespeichert solange sie alle";
+$lang[faq2b] = "Tage einmal heruntergeladen werden";
+$lang[faq3a] = "Sie können eine Datei alle";
+$lang[faq3b] = "Minuten hochladen.";
+$lang[faq3c] = "Sie können eine Datei alle";
+$lang[faq3d] = "Minuten runterladen.";
+$lang[faq4a] = "Dateien kleiner als";
+$lang[faq4b] = "MB haben keine Up- oder Downloadlimits.";
+$lang[faq5a] = "Besuche einfach";
+$lang[faq5b] = "und lade Dir den Mini File Host herunter. Er ist einfach zu installieren und zu benutzen!";
+
+// search.php
+$lang[search] = "Suche";
+$lang[searchhits] = "Treffer gefunden für";
+$lang[nosearchhits] = "Leider keinen Treffer gefunden für";
+?>
